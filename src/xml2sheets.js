5071f733a2a9cb3fd06b4a13334fc2ffce88415e
ttsukagoshi <55706659+ttsukagoshi@users.noreply.github.com>
2021-01-10 23:19:12 +0900

    Add dialog.html

--- src/xml2sheets.js
+++ src/xml2sheets.js
@@ -12,6 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function myFunction() {
-  
+function onOpen() {
+
+}
+
+function showDialog() {
+  var html = HtmlService.createHtmlOutputFromFile('dialog');
+  SpreadsheetApp.getUi().showModalDialog(html, "Upload XML");
 }
